103e995f2bae0d0db9fa1bbe2c9887c9aa3fceff
Farhad rasouli <rasouli.farhad@gmail.com>
2023-12-10 21:47:37 +0330

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { slideInAnimation } from './animations';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [slideInAnimation]
 })
 export class AppComponent {
   title = 'another-demo-app';
@@ -35,5 +38,9 @@ export class AppComponent {
         resolve();
       }, 2000);
     });
+  }
+
+  getAnimationData(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
 }
